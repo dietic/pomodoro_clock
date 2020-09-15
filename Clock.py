a13e0e7db2a7467159e6edcaf3ef246b2ce39350
Diego <dierios93@gmail.com>
2020-09-15 00:53:19 -0500

    Added break and speed

--- Clock.py
+++ Clock.py
@@ -1,17 +1,35 @@
+import time
+import sys
+
+
 class Clock:
-    def __init__(self, t, l="m"):
-        self.t = t
-        self.l = l
+    def __init__(self, t):
+        self.time = t * 60
+        self.current_time = 0
+        self.elapsed_time = 0
+        self.pause_time = 1 * 60
+        """ TODO: Delete this when finished """
+        self.time_speed = 20
 
-    def get_time_in_seconds(self):
-        switcher = {
-            "s": self.t,
-            "h": self.t*60*60,
-            "m": self.t*60
-        }
-        return switcher.get(self.l, "error")
+    def pause_clock(self, t):
+        time.sleep(t)
+
+    def start_clock(self):
+        while self.current_time <= self.time:
+            print(self.elapsed_time)
+            self.elapsed_time += 1
+            self.current_time += 1
+            time.sleep(1/self.time_speed)
+            if((self.elapsed_time % self.pause_time == 0)):
+                print("--BREAK start--")
+                pause_elapsed_time = 0
+                while pause_elapsed_time <= self.pause_time:
+                    print(pause_elapsed_time)
+                    time.sleep(1/self.time_speed)
+                    pause_elapsed_time += 1
+                    self.current_time += 1
+                print("--BREAK end--")
 
 
-clock = Clock(2231, "h")
-
-print(clock.get_time_in_seconds())
+clock = Clock(30)
+clock.start_clock()
